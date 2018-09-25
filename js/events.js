8630b13168706aced7897b3ac0bf7a405e72dd19
Andrew Johnson <ajohnson.uva@gmail.com>
2018-09-25 22:59:28 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
 //define functions here
 
+function getIt() {
+  $('p').on("click", function() {
+    alert('Hey!');
+  })
+}
+
+function frameIt() {
+  $('img').on("load", function() {
+    var img1 = $('img');
+    img1.add('.tasty');
+  })
+}
+
+function pressIt() {
+  $('input').on("keydown", function() {
+    if (key.which === 71) {
+      alert('You have pressed the G key');
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on("submit", function() {
+    alert("Your form is going to be submitted now.");
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
+
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
